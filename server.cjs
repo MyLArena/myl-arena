3636d9e85d126fcbde34242a91cf9e8ca1e6da40
MyLArena <myl.arena02@gmail.com>
2026-08-06 22:51:10 -0400

    actualizacion

--- server.cjs
+++ server.cjs
@@ -104,8 +104,8 @@ io.on('connection', (socket) => {
   });
 });
 
-// --- 2. RUTA COMODÍN PARA SPA (Corregida con /*) ---
-app.get('/*', (req, res) => {
+// --- 2. RUTA COMODÍN CON EXPRESIÓN REGULAR (Solución definitiva para SPA) ---
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
